f48adcc3db7ac3788c302e8e839cff429f6ac9b9
Activa Consultora <activaconsultanqn@gmail.com>
2025-03-31 12:32:33 -0300

    Solución definitiva: archivos corregidos

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,3 @@
-
-import React from "react";
-import { DashboardGraficos } from "./DashboardGraficos";
-
 export default function App() {
-  return (
-    <div className="bg-gray-100 min-h-screen p-6">
-      <h1 className="text-3xl font-bold text-center mb-8">Dashboard de Actividades</h1>
-      <DashboardGraficos />
-    </div>
-  );
+  return <h1>Mi Aplicación</h1>;
 }
